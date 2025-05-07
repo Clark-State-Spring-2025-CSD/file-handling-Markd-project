254460bad377e94af638cb9b069fc0b3493c8179
Markd-project <markd1885929@gmail.com>
2025-05-07 02:50:37 -0400

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -3,3 +3,15 @@
 #For example, wow is a paldindrome.
 #A different file wile be used for grading.
 #Correct answer for this file: 
+
+Pal = 0
+Row =""
+
+with open("words.txt", "r") as i:
+    for z in i:
+        Wor = z.strip()
+        Row = Wor[::-1]
+        if Wor == Row:
+            Pal += 1
+
+print(f"There are {Pal} palindromes")
